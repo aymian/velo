3a89353019b734f412d07645644857ad1794b0dc
aymian <aymian2050@gmail.com>
2026-02-22 15:30:20 +0200

    How please

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vyohajurajknstksckik.supabase.co';
-const supabaseAnonKey = 'sb_publishable_A6nwwEgjc3OR2PlotQG0Dg_h2u05Jjg';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
